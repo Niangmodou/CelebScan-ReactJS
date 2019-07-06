17531b2835a4ac9d9defb4c97be5972d698c28ea
Modou <niangmodou100@gmail.com>
2019-07-06 10:18:55 -0500

    Switched Branch

--- src/App.js
+++ src/App.js
@@ -19,21 +19,34 @@ const app = new Clarifai.App({
 class App extends Component{
     constructor(){
         super();
+        this.modelId = "e466caa0619f444ab97497640cefc4dc";
         this.state = { //Used to track the current state of the Url box
             urlInput: ""
         }
+    }
+    
+    buttonSumbit = () => {
+        app.models.predict(this.modelId, "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Jay-Z_%40_Shawn_%27Jay-Z%27_Carter_Foundation_Carnival_%28crop_2%29.jpg/220px-Jay-Z_%40_Shawn_%27Jay-Z%27_Carter_Foundation_Carnival_%28crop_2%29.jpg").then(
+        function(response) {
+            console.log(response)
+        },
+        function(err) {
+            console.log(err)
+        }
+        );
     }
 
     //FUNCTION NEEDS WORK!
     textChange = (event) => {
         let currUrl = event.target.value; //Function used to retrieve the current state of the Url box
         console.log(currUrl);
-        this.setState((this.urlInput: currUrl));
+        //this.setState((this.urlInput: currUrl));
     }
 
     render(){
         return (
             <div>
+                this.buttonSumbit();
                 <Header />
                 <UrlSearch textChange= {this.textChange}/> 
                 <CelebrityImage />
