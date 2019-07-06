34f4a6ddc0a5c678f481eba356b6d816182db60f
Jin Kim <jinsungkim1210@gmail.com>
2019-07-05 20:28:30 -0700

    Test commit #1.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// Test Commit: Making sure nothing goes wrong when I edit.
 ReactDOM.render(<App />, document.getElementById('root'));
 
