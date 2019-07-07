eed52ae4c18c58615f3ea0cc9185a79f4a84fa6c
Modou <niangmodou100@gmail.com>
2019-07-06 23:16:15 -0500

    Updates

---
+++ precache-manifest.25c994ffb02ccdbfaa9f09f9c0ea8d2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5777919d57ef066a62dddd1ffbf9a0aa",
+    "url": "/CelebScan-ReactJS/index.html"
+  },
+  {
+    "revision": "a01262fa4a8f35997e33",
+    "url": "/CelebScan-ReactJS/static/css/main.708534d2.chunk.css"
+  },
+  {
+    "revision": "316bade66c0c3631b8dc",
+    "url": "/CelebScan-ReactJS/static/js/2.135086de.chunk.js"
+  },
+  {
+    "revision": "a01262fa4a8f35997e33",
+    "url": "/CelebScan-ReactJS/static/js/main.d53dfa5f.chunk.js"
+  },
+  {
+    "revision": "62cc3e2efe2b892750c2",
+    "url": "/CelebScan-ReactJS/static/js/runtime~main.1b2d4e9e.js"
+  }
+]);
