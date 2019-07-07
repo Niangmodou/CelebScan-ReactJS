16cdb9b203d538c5be609f06835c618ed5070aee
Modou <niangmodou100@gmail.com>
2019-07-06 20:33:12 -0500

    Added build folder

---
+++ build/precache-manifest.55eaee2abc07a6c10b0b537cb0d3e8f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde93a2f6343b5ad9e1c1bf00fad04d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a01262fa4a8f35997e33",
+    "url": "/static/css/main.708534d2.chunk.css"
+  },
+  {
+    "revision": "edd7d490f61d8ed51ad7",
+    "url": "/static/js/2.0104914e.chunk.js"
+  },
+  {
+    "revision": "a01262fa4a8f35997e33",
+    "url": "/static/js/main.d53dfa5f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
