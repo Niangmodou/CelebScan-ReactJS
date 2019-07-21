e45e50c5e3206b6ebc66f0fb20760978aefcdf09
Modou Niang <43767534+Niangmodou@users.noreply.github.com>
2019-07-21 16:25:50 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     
     return(
-        <h3 className="footer">Made with ♡ by Modou Niang</h3>
+        <h3 className="footer">Made with ♡ by Modou Niang X Jin Kim</h3>
     );
 
 }
